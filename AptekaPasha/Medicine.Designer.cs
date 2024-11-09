64eb64cd9bf3fa6878a4624e8c9b0c8ef5ff9a43
personinsey@gmail.com <personinsey@gmail.com>
2024-11-09 06:38:41 +0300

    Добавьте файлы проекта.

---
+++ AptekaPasha/Medicine.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace AptekaPasha
+{
+    partial class Medicine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMedicine = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanelButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.medicineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medicineToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMedicine)).BeginInit();
+            this.flowLayoutPanelButtons.SuspendLayout();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMedicine
+            // 
+            this.dataGridViewMedicine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMedicine.Location = new System.Drawing.Point(254, 0);
+            this.dataGridViewMedicine.Name = "dataGridViewMedicine";
+            this.dataGridViewMedicine.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewMedicine.TabIndex = 0;
+            // 
+            // flowLayoutPanelButtons
+            // 
+            this.flowLayoutPanelButtons.Controls.Add(this.btnAdd);
+            this.flowLayoutPanelButtons.Controls.Add(this.btnEdit);
+            this.flowLayoutPanelButtons.Controls.Add(this.btnDelete);
+            this.flowLayoutPanelButtons.Controls.Add(this.btnLoadData);
+            this.flowLayoutPanelButtons.Location = new System.Drawing.Point(215, 418);
+            this.flowLayoutPanelButtons.Name = "flowLayoutPanelButtons";
+            this.flowLayoutPanelButtons.Size = new System.Drawing.Size(325, 31);
+            this.flowLayoutPanelButtons.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(84, 3);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(165, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(246, 3);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadData.TabIndex = 3;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click_1);
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.medicineToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(784, 24);
+            this.MenuStrip.TabIndex = 2;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // medicineToolStripMenuItem
+            // 
+            this.medicineToolStripMenuItem.Name = "medicineToolStripMenuItem";
+            this.medicineToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.medicineToolStripMenuItem.Text = "Medicine";
+            this.medicineToolStripMenuItem.Click += new System.EventHandler(this.medicineToolStripMenuItem_Click);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.medicineToolStripMenuItem1,
+            this.customerToolStripMenuItem,
+            this.orderToolStripMenuItem,
+            this.orderItemToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // medicineToolStripMenuItem1
+            // 
+            this.medicineToolStripMenuItem1.Name = "medicineToolStripMenuItem1";
+            this.medicineToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.medicineToolStripMenuItem1.Text = "Medicine";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // orderToolStripMenuItem
+            // 
+            this.orderToolStripMenuItem.Name = "orderToolStripMenuItem";
+            this.orderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.orderToolStripMenuItem.Text = "Order";
+            // 
+            // orderItemToolStripMenuItem
+            // 
+            this.orderItemToolStripMenuItem.Name = "orderItemToolStripMenuItem";
+            this.orderItemToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.orderItemToolStripMenuItem.Text = "OrderItem";
+            // 
+            // Medicine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.flowLayoutPanelButtons);
+            this.Controls.Add(this.dataGridViewMedicine);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Medicine";
+            this.Text = "Medicine";
+            this.Load += new System.EventHandler(this.Medicine_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMedicine)).EndInit();
+            this.flowLayoutPanelButtons.ResumeLayout(false);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewMedicine;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelButtons;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem medicineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medicineToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orderItemToolStripMenuItem;
+    }
+}
---
+++ AptekaPasha/Medicine.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AptekaPasha
+{
+    public partial class Medicine : Form
+    {
+        private readonly string connectionString = "Data Source=ACER;Initial Catalog=BD_apteka;Integrated Security=True;Encrypt=False";
+        public Medicine()
+        {
+            InitializeComponent();
+        }
+
+        private void Medicine_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+            LoadMedicineData();  // загрузка данных
+
+            dataGridViewMedicine.Dock = DockStyle.Fill;
+
+            dataGridViewMedicine.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // вскрыл колонку Id
+            if (dataGridViewMedicine.Columns.Contains("Id"))
+            {
+                dataGridViewMedicine.Columns["Id"].Visible = false;
+            }
+
+            flowLayoutPanelButtons.Dock = DockStyle.Bottom;
+            flowLayoutPanelButtons.FlowDirection = FlowDirection.LeftToRight;
+            flowLayoutPanelButtons.AutoSize = true;
+            flowLayoutPanelButtons.HorizontalScroll.Visible = false;
+            flowLayoutPanelButtons.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        }
+
+        // загрузка данных
+        private void LoadMedicineData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Medicine";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridViewMedicine.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+                }
+            }
+        }
+
+        // добавление новой записи
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridViewMedicine.CurrentRow == null)
+            {
+                MessageBox.Show("Выберите запись для добавления.");
+                return;
+            }
+
+            string name = Convert.ToString(dataGridViewMedicine.CurrentRow.Cells["Name"].Value);
+            string description = Convert.ToString(dataGridViewMedicine.CurrentRow.Cells["Description"].Value);
+            decimal price = Convert.ToDecimal(dataGridViewMedicine.CurrentRow.Cells["Price"].Value);
+            int stockQuantity = Convert.ToInt32(dataGridViewMedicine.CurrentRow.Cells["StockQuantity"].Value);
+            DateTime expiryDate = Convert.ToDateTime(dataGridViewMedicine.CurrentRow.Cells["ExpiryDate"].Value);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Medicine (Name, Description, Price, StockQuantity, ExpiryDate) VALUES (@Name, @Description, @Price, @StockQuantity, @ExpiryDate)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Description", description);
+                        command.Parameters.AddWithValue("@Price", price);
+                        command.Parameters.AddWithValue("@StockQuantity", stockQuantity);
+                        command.Parameters.AddWithValue("@ExpiryDate", expiryDate);
+
+                        command.ExecuteNonQuery();
+                    }
+                    LoadMedicineData(); // перезагрузка
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при добавлении записи: " + ex.Message);
+                }
+            }
+        }
+
+        // редактирование записи
+        private void btnEdit_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridViewMedicine.CurrentRow == null)
+            {
+                MessageBox.Show("Выберите запись для редактирования.");
+                return;
+            }
+
+            int id = Convert.ToInt32(dataGridViewMedicine.CurrentRow.Cells["Id"].Value);
+            string name = Convert.ToString(dataGridViewMedicine.CurrentRow.Cells["Name"].Value);
+            string description = Convert.ToString(dataGridViewMedicine.CurrentRow.Cells["Description"].Value);
+            decimal price = Convert.ToDecimal(dataGridViewMedicine.CurrentRow.Cells["Price"].Value);
+            int stockQuantity = Convert.ToInt32(dataGridViewMedicine.CurrentRow.Cells["StockQuantity"].Value);
+            DateTime expiryDate = Convert.ToDateTime(dataGridViewMedicine.CurrentRow.Cells["ExpiryDate"].Value);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "UPDATE Medicine SET Name = @Name, Description = @Description, Price = @Price, StockQuantity = @StockQuantity, ExpiryDate = @ExpiryDate WHERE Id = @Id";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Description", description);
+                        command.Parameters.AddWithValue("@Price", price);
+                        command.Parameters.AddWithValue("@StockQuantity", stockQuantity);
+                        command.Parameters.AddWithValue("@ExpiryDate", expiryDate);
+
+                        command.ExecuteNonQuery();
+                    }
+                    LoadMedicineData(); // нужно больше перезагрузок
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при редактировании записи: " + ex.Message);
+                }
+            }
+        }
+
+        // удаление записи
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridViewMedicine.CurrentRow == null)
+            {
+                MessageBox.Show("Выберите запись для удаления.");
+                return;
+            }
+
+            int id = Convert.ToInt32(dataGridViewMedicine.CurrentRow.Cells["Id"].Value);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Medicine WHERE Id = @Id";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.ExecuteNonQuery();
+                    }
+                    LoadMedicineData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при удалении записи: " + ex.Message);
+                }
+            }
+        }
+
+        // обновление данных
+        private void btnLoadData_Click_1(object sender, EventArgs e)
+        {
+            LoadMedicineData();
+        }
+
+        private void medicineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
